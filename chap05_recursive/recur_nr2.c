d0e2a7868b8586920ae3ab3d4767d69528a19083
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-26 02:41:07 +0900

    eliminate recursive function using stack

---
+++ chap05_recursive/recur_nr2.c
@@ -0,0 +1,23 @@
+/*--- 재귀 호출을 제거한 recur 함수 ---*/
+#include "../chap04_stack_queue/IntStack.h"
+#include "../chap04_stack_queue/IntStack.c"
+
+void recur(int n) {
+	IntStack stk;			//스택 
+	Initialize(&stk, 100);
+	
+	Top :
+		if (n > 0) {
+			Push(&stk, n);	// n의 값을 푸시
+			n = n - 1;
+			goto Top; 
+		}
+		if (!IsEmpty(&stk)) {	// 스택이 비어 있지 않으면 
+			Pop(&stk, &n);		// 값을 저장했던 n을 팝
+			printf("%d\n", n);
+			n = n - 2;
+			goto Top; 
+		}
+		
+	Terminate(&stk);
+} 
