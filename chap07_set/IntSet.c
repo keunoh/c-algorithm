35383528da60c3479c8dbab0b16b2a8cad01461e
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-05 01:00:43 +0900

    feature: 'set' header & functions

--- chap07_set/IntSet.c
+++ chap07_set/IntSet.c
@@ -1,53 +1,39 @@
-//int형 집합 IntSet(헤더)
-#ifndef ___IntSet
-#define ___IntSet
+//int형 집합 IntSet(소스)
 
-//int형 집합을 관리하는 구조체
-typedef struct {
-	int max;	//집합의 크기
-	int num;	//집합의 원소 개수
-	int *set;	//집합 본체의 배열(의 첫 번째 요소에 대한 포인터) 
-} IntSet;
+#include <stdio.h>
+#include <stdlib.h>
+#include "IntSet.h"
 
-//집합의 초기화
-int Initialize(IntSet *s, int max);
+//집합 초기화
+int Initialize(IntSet *s, int max) {
+	s->num = 0;
+	if((s->set = calloc(max, sizeof(int))) == NULL) {
+		s->max = 0;	//배열 생성에 실패
+		return -1; 
+	}
+	s->max = max;
+	return 0;
+}
 
 //집합 s에 n이 들어 있는지 확인
-int IsMember(const IntSet *s, int n);
+int IsMember(const IntSet *s, int n) {
+	int i;
+	for(i = 0; i < s->num; i++)
+		if(s->set[i] == n)
+			return i;	//들어있음(인덱스를 반환)
+	return -1;			//들어있지 않음 
+} 
 
 //집합 s에 n을 추가
-void Add(IntSet *s, int n);
+void Add(IntSet *s, int n) {
+	if(s->num < s->max && IsMember(s, n) == -1)	//들어있지 않으면
+		s->set[s->num++] = n;		//배열 끝에 n을 추가
+}
 
 //집합 s에서 n을 삭제
-void Remove(IntSet *s, int n);
-
-//집합 s에 넣을 수 있는 최대의 원소 개수를 반환
-int Capacity(const IntSet *s);
-
-//집합 s의 원소 개수
-int Size(const IntSet *s);
-
-//집합 s2를 s1에 대입
-void Assign(IntSet *s1, const IntSet *s2);
-
-//집합 s1과 s2가 같은지 확인
-int Equal(const IntSet *s1, const IntSet *s2);
-
-//집합 s2와 s3의 합집합을 s1에 대입
-IntSet *Union(IntSet *s1, const IntSet *s2, const IntSet *s3);
-
-//집합 s2와 s3의 교집합을 s1에 대입
-IntSet *Intersection(IntSet *s1, const IntSet *s2, const IntSet *s3);
-
-//집합 s2에서 s3를 뺀 차집합을 s1에 대입
-IntSet *Difference(IntSet *s1, const IntSet *s2, const IntSet *s3);
-
-//집합 s의 모든 원소를 출력
-void Print(const IntSet *s);
-
-//집합 s의 모든 원소를 출력(줄 바꿈 문자 포함)
-void PrintLn(const IntSet *s);
-
-//집합 종료
-void Terminate(IntSet *s);
-#endif 
+void Remove(IntSet *s, int n) {
+	int idx;
+	if((idx = IsMember(s, n)) != -1) {
+		s->set[idx] = s->set[--s->num];	//마지막 요소를 삭제 위치로 옮김 
+	}
+} 
