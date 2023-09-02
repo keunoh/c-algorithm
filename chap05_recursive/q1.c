07be326d0e23aec5b97f8f32778c00b591ab1802
keunoh <rmsdhcjswo1@gmail.com>
2023-09-02 11:42:03 +0900

    factorial without recursive call

---
+++ chap05_recursive/q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int n = 5;
+	int sum = 1;
+	
+	5 * 4 * 3 * 2 * 1;
+	
+	for(i = 1; i <= n; i++) {
+		sum *= i;
+	}
+	
+	printf("sum = %d\n", sum);
+	
+}
