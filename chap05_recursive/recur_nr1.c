88169e646de8275797aca46f9b2099d31b0b1c6b
keunoh <rmsdhcjswo1@gmail.com>
2023-09-20 21:18:53 +0900

    Elimination of Recursive call using Stack

--- chap05_recursive/recur_nr1.c
+++ chap05_recursive/recur_nr1.c
@@ -1,5 +1,6 @@
 /*--- 함수 recur(꼬리 재귀를 제거) ---*/
-void recur(int n) {
+void recur(int n)
+{
 	Top :
 		if(n > 0) {
 			recur(n - 1);
@@ -7,4 +8,6 @@ void recur(int n) {
 			n = n - 2;
 			goto Top;
 		}
-} 
+}
+
+/* n의 값을 n - 2로 업데이트하고 함수의 시작 지점으로 돌아갑니다. */
