904502dc6ca2eac24c558979233b9d358f26237e
keunoh <rmsdhcjswo1@gmail.com>
2023-11-05 12:40:39 +0900

    LinkedList

--- chap09_list/LinkedList.c
+++ chap09_list/LinkedList.c
@@ -150,20 +150,170 @@ void InsertRear (List *list, const Member *x)
 		이때 노드 G의 다음 노드에 널을 대입합니다. 
 */
 
+/*--- 머리 노드를 삭제하는 RemoveFront 함수 ---*/
+void RemoveFront (List *list)
+{
+	if (list->head != NULL) {
+		Node *ptr = list->head->next;	/* 두 번째 노드에 대한 포인터 */
+		free(list->head);				/* 머리 노드를 해제 */
+		list->head = list->crnt = ptr;	/* 새로운 머리 노드 */
+	}
+}
 
+/* [ 머리 노드를 삭제하는 RemoveFront 함수 ]
+	RemoveFront 함수는 머리 노드를 삭제하는 함수입니다.
+	리스트가 비어 있지 않은 경우(list-> head != NULL)에만 삭제를 실행합니다.
+	a의 리스트에서 머리 노드 A를 삭제하고 나면 b상태가 됩니다.
+	머리 노드에 대한 포인터 list->head에 두 번째 노드 B에 대한 포인터 list->head->next를 대입하여 list->head가 가리키는 노드를 B로 업데이트합니다.
+	그리고 삭제하기 전의 머리 노드 A의 메모리 영역을 해제합니다.
+	
+	만약 리스트에 노드가 1개만 있어도 오류없이 삭제할 수 있습니다.
+	삭제하기 전의 머리 노드는 꼬리 노드이기 때문에 다음 노르를 가리키는 list->head->next의 값은 널입니다.
+	널을 list->head에 대입하면 리스트는 빈 상태가 됩니다. 
+*/
 
+/*--- 꼬리에 노드를 삭제하는 함수 ---*/
+void RemoveRear (List *list)
+{
+	if (list->head != NULL) {
+		if((list->head)->next == NULL)	/* 노드가 1개만 있는 경우*/
+			RemoveFront(list);			/* 머리 노드를 삭제 */
+		else {
+			// 1
+			// while문이 종료되면 ptr은 꼬리 노드를 가리키고,
+			// pre는 꼬리 노드로부터 두 번째 노드를 가리킵니다. 
+			Node *ptr = list->head;
+			Node *pre;
+			while(ptr->next != NULL) {
+				pre = ptr;
+				ptr = ptr->next;
+			}
+			// 2
+			pre->next = NULL;			/* pre는 꼬리 노드로부터 두 번째 노드 */
+			free(ptr);					/* ptr은 꼬리 노드 */
+			list->crnt = pre;
+		}
+	}
+}
 
+/* [ 꼬리 노드를 삭제하는 RemoveRear 함수 ]
+	RemoveRear 함수는 머리 노드를 삭제하는 함수입니다.
+	리스트에 노드가 몇 개 있는지에 따라 그 경우에 해당하는 작업을 수행합니다. 
+	
+	1. 리스트에 노드가 1개만 있는 경우
+		머리 노드를 삭제하는 것과 같습니다. RemoveFront 함수로 처리합니다.
+	2. 리스트에 노드가 2개 이상 있는 경우
+		그림 9-11을 통해 삭제 과정을 구체적으로 설명했습니다.
+		
+	// 1
+		`꼬리노드`와 `꼬리 노드로부터 두 번째 노드`를 찾습니다.
+		스캔하는 방법은 InsertRear 함수와 비슷하지만 현재 스캔하고 있는 노드의 `앞에 있는 노드`를 가리키는 변수 pre를 추가한 점이 다릅니다.
+	// 2
+		꼬리 노드부터 두 번째 노드 E의 다음을 가리키는 포인터에 널을 대입하고 꼬리 노드 F의 메모리 영역을 해제합니다. 
+*/
 
+/*--- 선택한 노드를 삭제하는 함수 ---*/
+void RemoveCurrnet (List *list)
+{
+	if (list->head != NULL) {
+		if (list->crnt == list->head)
+			RemoveFront(list);
+		else {
+			// 1
+			Node *ptr = list->head;
+			while(ptr->next != list->crnt)
+				ptr = ptr->next;
+			// 2
+			ptr->next = list->crnt->next;
+			free(list->crnt);
+			list->crnt = ptr;
+		}
+	}
+}
 
+/* [ 선택한 노드를 삭제하는 RemoveCurrent 함수 ]
+	RemoveCurrent 함수는 현재 선택한 노드(list->crnt)가 가리키는 노드를 삭제하는 함수입니다.
+	선택한 노드가 머리 노드인지 아닌지에 따라 다음의 작업을 수행합니다. 
+	
+	1. crnt가 머리 노드인 경우 
+		머리 노드를 삭제하면 됩니다. RemoveFront 함수로 처리합니다. 
+	2. crnt가 머리 노드가 아닌 경우 
+		그림 9-12를 통해 삭제 과정을 구체적으로 설명했습니다. 
+		
+	// 1
+		선택한 노드의 앞 노드를 찾슷빈다. while문은 머리 노드부터 스캔을 시작합니다.
+		선택한 노드 ptr의 다음 노드를 가리키는 포인터 ptr->next가 list->crnt와 같을 때 까지 반복합니다.
+		while문이 종료되고 난 다음 ptr이 가리키는 노드는 삭제하기 위해 선택한 노드 D의 앞쪽 노드인 노드 C가 됩니다. 
+	// 2
+		삭제하기 위해 선택한 노드 D의 다음 노드 포인터 list->crnt->next를 노드 C의 다음 노드 포인터 ptr->next에 대입합니다.
+		그러면 노드 C의 다음 노드 포인터가 가리키는 노드가 노드 E로 업데이트 됩니다.
+		그런 다음 노드 D의 메모리 영역을 해제합니다. 
+*/
 
+/*--- 모든 노드를 삭제 ---*/
+void Clear (List *list)
+{
+	while(list->head != NULL)	/* 텅 빌 때까지 */
+		RemoveFront(list);		/* 머리 노드를 삭제 */
+	list->crnt = NULL;
+}
 
+/* [ 모든 노드를 삭제하는 Clear 함수 ]
+	Clear 함수는 연결 리스트의 모든 노드를 삭제하는 함수입니다.
+	연결 리스트가 완전히 텅 빈 상태(head == NULL)가 될 때까지 머리 요소의 삭제 작업을 반복합니다.
+	// 모든 노드를 삭제하면 리스트가 완전히 빈 상태가 됩니다. 따라서 crnt의 값도 NULL로 업데이트 됩니다. 
+	
+*/
 
+/*--- 선택한 노드의 데이터를 출력 ---*/
+void PrintCurrent (const List *list)
+{
+	if (list->crnt == NULL)
+		printf("선택한 노드가 없습니다.")
+	else 
+		PrintMember(&list->crnt->data);
+}
 
+/*--- 선택한 노드의 데이터를 출력(줄 바꿈 문자 포함) ---*/
+void PrintLnCurrent (const List *list)
+{
+	PrintCurrent(list);
+	putchar('\n');
+}
 
+/* [ 선택한 노드의 데이터를 출력하는 PrintCurrent / PrintLnCurrent 함수
+	PrintCurrent, PrintLnCurrent 함수는 선택한 노드의 데이터를 출력하는 함수입니다.
+	선택한 노드가 없는 경우(list->crnt == NULL)에는 `선택한 노드가 없습니다.`라고 출력합니다.  
+*/
 
+void Print (const List *list) 
+{
+	if (list->head == NULL)
+		puts("There is no Node.");
+	else {
+		Node *ptr = list->head;
+		puts("[ View All ]");
+		while(ptr != NULL) {
+			PrintLnMember(&ptr->data);
+			ptr = ptr->next;		/* 다음 노드를 선택 */
+		}
+	}
+}
 
+/* [ 리스트의 모든 노드를 출력하는 Print 함수 ]
+	Print 함수는 리스트의 모든 노드를 순서대로 출력하는 함수입니다.
+	머리 노드부터 꼬리 노드까지 포인터 ptr이 가리키는 데이터를 출력합니다. 
+*/
 
+void Terminate (List *list)
+{
+	Clear(list);					/* 모든 노드를 삭제 */
+}
 
+/* [ 연결 리스트를 종료하는 Terminate 함수 ]
+	Terminate 함수는 연결 리스트를 종료하는 함수입니다.
+	모든 노드를 삭제하는 Clear 함수를 호출합니다. 
+*/
 
 
 
