7ca0bb97782c43ac28ec393f6b1d5b15ba84a3da
kaltz <rmsdhcjswo1@gmail.com>
2024-01-20 22:02:18 +0900

    3일차인데 재귀는 보통 한 번 메인에서 실행 스스로 2번

---
+++ solve_recursion/6603.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int S[13];
+int lotto[6];
+int k;
+
+void getCombi(int start, int depth){
+    if(depth==6){
+        for(int i=0;i<6;i++)
+            cout << lotto[i] << " ";
+        cout << endl;
+        return;
+    }
+    for(int i=start;i<k;i++){
+        lotto[depth] = S[i];
+        getCombi(i+1,depth+1);
+    }
+}
+
+int main(){
+    while(1){
+        cin >> k;
+        if(k==0) break;
+        for(int i=0;i<k;i++)
+            cin >> S[i];
+        getCombi(0,0);
+        cout << endl;
+    }
+}
