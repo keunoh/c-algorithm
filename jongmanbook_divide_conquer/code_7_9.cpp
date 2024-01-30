2fa90f8b3aeb21b791d4f2ebe3d4945867eb390e
kaltz <rmsdhcjswo1@gmail.com>
2024-01-30 22:24:41 +0900

    12일차: 큰 수의 곱셈을 이용한다. 각 자릿 수의 값은 C의 인덱스로 들어가고 뒤에서 앞으로 가는 방식이다

---
+++ jongmanbook_divide_conquer/code_7_9.cpp
@@ -0,0 +1,17 @@
+//카라츠바의 빠른 곱셈을 이용해 팬미팅 문제를 해결하는 함수
+#include <vector>
+using namespace std;
+
+int hugs(const string& members, const string& fans) {
+    int N = members.size(), M = fans.size();
+    vector<int> A(N), B(M);
+    for(int i = 0; i < N; ++i) A[i] = (members[i] == 'M');
+    for(int i = 0; i < M; ++i) B[M-i-1] = (fans[i] == 'M');
+    //karatsuba 알고리즘에서 자리 올림은 생략한다.
+    vector<int> C = karatsuba(A, B);
+    int allHugs = 0;
+    for(int i = N-1; i < M; ++i)
+        if(C[i] == 0)
+            ++allHugs;
+    return allHugs;
+}
