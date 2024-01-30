d78b57e69370bd5f76d95b386065c9289f3ed975
kaltz <rmsdhcjswo1@gmail.com>
2024-01-30 21:56:32 +0900

    12일차: 2중 for문, ret는 사각형의 넓이 공식 -> 이것을 알아내야 한다

---
+++ jongmanbook_divide_conquer/code_7_7.cpp
@@ -0,0 +1,18 @@
+//울타리 잘라내기 문제를 해결하는 O(n^2) 알고리즘
+
+#include <vector>
+using namespace std;
+//판자의 높이를 담은 배열 h[]가 주어질 때 사각형의 최대 너비를 반환한다.
+int bruteForce(const vector<int>& h) {
+    int ret = 0;
+    int N = h.size();
+    //가능한 모든 left, right 조합을 순회한다.
+    for(int left = 0; left < N; ++left) {
+        int minHeight = h[left];
+        for(int right = left; right < N; ++right) {
+            minHeight = min(minHeight, h[right]);
+            ret = max(ret, (right - left + 1) * minHeight);
+        }
+    }
+    return ret;
+}
