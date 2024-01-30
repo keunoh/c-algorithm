22f2ca7aaf2fea7fb6357b58612991d3481f19da
kaltz <rmsdhcjswo1@gmail.com>
2024-01-30 21:37:38 +0900

    12일차: 기저사례는 항상 존재하고, 쿼드트리는 재귀를 4번 호출했다

---
+++ jongmanbook_divide_conquer/code_7_5.cpp
@@ -0,0 +1,24 @@
+//쿼드 트리 압축을 해제하는 재귀 호출 알고리즘
+
+#include <iterator>
+using namespace std;
+
+char decompressed[MAX_SIZE][MAX_SIZE];
+void decompress(string::iterator& it, int y, int x, int size) {
+    //한 글자를 검사할 때마다 반복자를 한 칸 앞으로 옮긴다.
+    char head = *(it++);
+    //기저 사례: 첫 글자가 b 또는 w인 경우
+    if(head == 'b' || head == 'w') {
+        for(int dy = 0; dy < size; ++dy)
+            for(int dx = 0; dx < size; ++dx)
+                decompress[y+dy][x+dx] = head;
+    }
+    else {
+        //네 부분을 각각 순서대로 압축 해제한다.
+        int half = size/2;
+        decompress(it, y, x, half);
+        decompress(it, y, x+half, half);
+        decompress(it, y+half, x, half);
+        decompress(it, y+half, x+half, half);
+    }
+}
