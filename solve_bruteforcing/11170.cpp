93445c4c154c7313012e0e357ccca77dc8c2cc3e
kaltz <rmsdhcjswo1@gmail.com>
2024-04-01 22:05:22 +0900

    38일차: 숫자문제더라도 문자열로 생각해볼 수 있다

---
+++ solve_bruteforcing/11170.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int test;
+    cin >> test;
+
+    for(int i = 0; i < test; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        int cnt = 0;
+        int sum = 0;
+
+            for(int i = a; i <= b; i++) {
+                string str = to_string(i);
+                for(int j = 0; j < str.length(); j++) {
+                    if(str[j] == '0') cnt++;
+                }
+            }
+
+            cout << cnt << endl;
+    }
+}
