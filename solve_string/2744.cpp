f684cd89c7abcde3c76752a3df9527df852e2759
keunoh <rmsdhcjswo1@gmail.com>
2025-02-16 23:47:26 +0900

    string: ASCII

---
+++ solve_string/2744.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string str;
+  cin >> str;
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] >= 65 && str[i] <= 90)
+    {
+      str[i] += 32;
+    }
+    else
+    {
+      str[i] -= 32;
+    }
+  }
+
+  cout << str;
+
+  return 0;
+}
