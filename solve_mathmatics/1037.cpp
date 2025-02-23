848dd1c1703af0b4c5d89e8d4baf80ded6dd987e
keunoh <rmsdhcjswo1@gmail.com>
2025-02-24 00:13:14 +0900

    yaksoo

---
+++ solve_mathmatics/1037.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int com(int i, int j)
+{
+  return i < j;
+}
+
+int main()
+{
+  int n, result;
+  cin >> n;
+  vector<int> num;
+
+  for (int i = 0; i < n; i++)
+  {
+    int N;
+    cin >> N;
+    num.push_back(N);
+  }
+
+  sort(num.begin(), num.end(), com);
+  if (num.size() != 1)
+  {
+    result = num[0] * num[num.size() - 1];
+  }
+  else
+  {
+    result = num[0] * num[0];
+  }
+
+  cout << result;
+}
