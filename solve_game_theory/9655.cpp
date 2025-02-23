b51eb730ffbb8604f282103a7a93d05b1c2817fc
keunoh <rmsdhcjswo1@gmail.com>
2025-02-23 23:38:31 +0900

    dol game

---
+++ solve_game_theory/9655.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  if (N % 2 == 1)
+  {
+    cout << "SK";
+  }
+  else
+  {
+    cout << "CY";
+  }
+
+  return 0;
+}
