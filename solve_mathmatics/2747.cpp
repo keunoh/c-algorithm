c4f85fadd361a3497314b1d516094ca28a3bc1c3
keunoh <rmsdhcjswo1@gmail.com>
2025-02-26 23:45:13 +0900

    pibonacci

---
+++ solve_mathmatics/2747.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  int DP[45];
+
+  cin >> n;
+
+  DP[0] = 0;
+  DP[1] = 1;
+
+  for (int i = 2; i <= n; i++)
+  {
+    DP[i] = DP[i - 1] + DP[i - 2];
+  }
+
+  cout << DP[n] << endl;
+
+  return 0;
+}
